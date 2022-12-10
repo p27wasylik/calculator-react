4556d3897fc7e2463a3ea187e9cd60048b9be851
Adrian Wasylik <p27wasylik@gmail.com>
2022-12-10 15:15:32 +0100

    add last version of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Label from './label';
+import Legend from './legend';
+import Fieldset from './fieldset';
+import Container from './container'
+import Header from './header';
+import Section from './section';
+import Paragraph from './paragraph';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header
+        titlePartOne="Kalkulator"
+        titleEmoji="®"
+        titlePartTwo="Walutowy"
+        extended="Stan na dzień 10.10.2022 r. godz. 22:00"
+
+      />
+      <Section>
+        <Paragraph />
+        <Container>
+          <Fieldset
+            legend={
+              <Legend
+                content="PLN » €"
+              />}
+            content1={
+              <Label
+                placeholder="PLN"
+                disable={false}
+              />
+            }
+            content2={
+              <Label
+                placeholder="EUR"
+                disable={true}
+              />
+            }
+
+          />
+          <Fieldset
+            legend={
+              <Legend
+                content="€ » PLN"
+              />
+            }
+            content1={
+              <Label
+                placeholder="EUR"
+                disable={false}
+              />
+            }
+            content2={
+              <Label
+                placeholder="PLN"
+                disable={true}
+              />
+            }
+          />
+        </Container>
+      </Section>
+</>
+
+  )
 }
 
 export default App;
