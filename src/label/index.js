86d1922e4a7743e54e8709387f920423afe2dcad
Adrian Wasylik <p27wasylik@gmail.com>
2022-12-13 16:15:59 +0100

    interaction

--- src/label/index.js
+++ src/label/index.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 
 const Label = ({ placeholder, placeholder2, sign }) => {
     const [currency, setCurrency] = useState("");
-    const multiply = { currency } * +4.86;
-    const divide = { currency } / +4.86;
+    
+
 
 
     return (
         <label>
+            
             <input
                 value={currency}
                 className="input"
@@ -18,16 +19,14 @@ const Label = ({ placeholder, placeholder2, sign }) => {
                 disable={false}
                 onChange={({ target }) => setCurrency(target.value)}
             />
+            
             <input
-                value={
-                    { sign } ? multiply : divide
-                }
+                value={{sign} ? {currency} * +4.86 : {currency} / +4.86}
                 className="input"
                 type="number"
                 placeholder={placeholder2}
                 step="0.1"
                 disable={true}
-                onChange={({ target }) => setCurrency(target.value)}
             />
         </label>
     );
